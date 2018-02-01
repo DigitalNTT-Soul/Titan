745fb7ad57f55fcf8e4865f046d1e0bb97782986
DigitalNTT-Soul <31822519+DigitalNTT-Soul@users.noreply.github.com>
2018-01-31 20:34:39 -0500

    Refined the debug module so that it now permanently logs whoever attempts to use the kill and reboot commands.

--- debug.lua
+++ debug.lua
@@ -1,21 +1,23 @@
 local function kill(client, logger, message, text)
-    print('kill command received')
     if message.author.id == client.owner.id then
+        logger:log(3 --[[info]], message.author.fullname .. ' called kill.')
         message:reply('Aww dammit...')
         client:setStatus('invisible')
         client:stop()
     else
+        logger:log(2 --[[warning]], message.author.fullname .. ' does not have permission to kill!')
         message:reply("Fuck you. You don't own me.")
     end
 end
 
 local function reboot(client, logger, message, text)
-    print('reboot command received')
     if message.author.id == client.owner.id then
+        logger:log(3 --[[info]], message.author.fullname .. ' called reboot.')
         message:reply('Rebooting')
         client:stop()
         os.execute('reboot.bat')
     else
+        logger:log(2 --[[warning]], message.author.fullname .. ' does not have permission to reboot!')
         message:reply("No thanks. I think I'm good.")
     end
 end
