9346cffe7ba341c506dd34d6213240f5d8d7f9c1
DigitalNTT-Soul <31822519+DigitalNTT-Soul@users.noreply.github.com>
2018-02-01 22:11:23 -0500

    debug.lua has to match up with the new name for the rebooting batch file.

--- debug.lua
+++ debug.lua
@@ -16,7 +16,7 @@ local function reboot(client, logger, message, text)
         message:reply('Rebooting')
         client:setStatus('invisible')
         client:stop()
-        os.execute('reboot.bat')
+        os.execute('boot.bat')
     else
         logger:log(2 --[[warning]], message.author.fullname .. ' does not have permission to reboot!')
         message:reply("No thanks. I think I'm good.")
