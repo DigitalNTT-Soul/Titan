922cacc15cffa26ce212217ab96944a75be96a89
DigitalNTT-Soul <31822519+DigitalNTT-Soul@users.noreply.github.com>
2018-02-02 17:11:40 -0500

    lazarus gimmick now works as intended. I MIGHT add some conditional client:setStatus() calls on top of that, just to add more giggles to this shit.

--- gimmix.lua
+++ gimmix.lua
@@ -1,7 +1,8 @@
 local function lazarus(client, logger, message, text)
-    messsage:reply("RUUUUUUUU-huhuhuuuuuuuuun!!!")
-    client:setStatus('dnd')
-    
+    local timer = require('timer')
+    message:reply("RUUUUUUUU-huhuhuuuuuuuuun!!!")
+    timer.sleep(300000)
+    message:reply('It occurs to me that ' .. message.author.name .. ' was just fucking with me when he mentioned my worst nightmare... He\'ll pay for that later.')
 end
 
 local function notbadforasorceror(client, logger, message, text)
