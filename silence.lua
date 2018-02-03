389753f6b77e02ea7121ad7e0c96e646c966ff01
DigitalNTT-Soul <31822519+DigitalNTT-Soul@users.noreply.github.com>
2018-02-03 09:42:49 -0500

    changed the name given in the reply scheme.
    also slightly changed up the if statement triggers and some variables:
    1. changed prefix to sPrefix
    2. added name (in the event that we ever change this bot's name)
    3. capitalization within the triggering message is irrelevant. as long as the command is spelled correctly, the reboot will commence.

--- silence.lua
+++ silence.lua
@@ -1,11 +1,12 @@
 local config = require('./config.lua')
 local discordia = require('discordia')
 local client = discordia.Client()
-local prefix = config[6]
+local sPrefix = config[6]
+local name = config[5]:lower()
 
 client:on('messageCreate', function (message)
-    if message.author.id == client.owner.id and message.content == prefix .. 'titan' then
-        message:reply('``TITAN`` Safe Rebooting. Stand By.')
+    if message.author.id == client.owner.id and message.content:lower() == sPrefix .. name then
+        message:reply('``SILENCE`` Safe Rebooting. Stand By.')
         os.execute('boot.bat')
     end
 end)
