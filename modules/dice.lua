3aad58db15fa399ed21fbecc070c66203ad8758f
DigitalNTT-Soul <31822519+DigitalNTT-Soul@users.noreply.github.com>
2018-02-12 10:18:31 -0500

    whitelist is now stable in the long term, as the whitelist is stored in separate database files now, rather than the header of the dice module

--- modules/dice.lua
+++ modules/dice.lua
@@ -1,12 +1,10 @@
 --[[whitelist storage]]
-
 local whitelistDefaults = {
     ['239563431919747073'] = 0.01, --Dylan
     ['252858420309262338'] = 0.01, --Sage
     ['277618305752367105'] = 0.06, --kaitlynn
     ['254784266959847425'] = 0.01, --michael
 }
-
 --[[helper functions]]
 
 
@@ -24,20 +22,25 @@ local function min(client, core, message)
 end
 
 local function superon(client, core, message)
-    if whitelist[message.author.id] and message.channel.type==1 then
-        if tonumber(message._text) and tonumber(message._text)>=1 and tonumber(message._text)<=100 then
-            whitelist[message.author.id] = tonumber(message._text) / 100
+    if whitelistDefaults[message.author.id] and message.channel.type==1 then
+        local new = tonumber(message._text)
+        if new and 1 <= new and new <= 100 then
+            file = io.open('./database/whitelist/' .. message.author.id, 'w')
+            file:write(new / 100)
+            file:close()
             message:reply("RNG cheat activated: You will now roll in the " .. message._text .. "th percentile.")
         else
-            message:reply("I'm sorry. You must enter a number between 1 and 100 (inclusive) in order to activate this cheat.")
+            message:reply("I'm sorry, you must enter a number between 1 and 100 (inclusive) in order to activate this cheat.")
         end
     end
     return nil
 end
 
 local function superoff(client, core, message)
-    if whitelist[message.author.id] and message.channel.type==1 then
-        whitelist[message.author.id] = whitelistDefaults[message.author.id]
+    if whitelistDefaults[message.author.id] and message.channel.type==1 then
+        local file = io.open('./database/whitelist/' .. message.author.id, 'w')
+        file:write(whitelistDefaults[message.author.id])
+        file:close()
         message:reply("RNG cheat deactivated: You will now roll normally.")
     end
     return nil
