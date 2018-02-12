6bf2fa3fe31d740409f39b232d667d1def37e247
DigitalNTT-Soul <31822519+DigitalNTT-Soul@users.noreply.github.com>
2018-02-11 21:52:41 -0500

    changed acon and acoff to superon and superoff, coded most of them, and wrote up the default values for the whitelist. I need to code the part where it will actually pull in the actual whitelists from the database... but progress is made.

--- modules/dice.lua
+++ modules/dice.lua
@@ -1,3 +1,16 @@
+--[[whitelist storage]]
+
+local whitelistDefaults = {
+    ['239563431919747073'] = 0.01, --Dylan
+    ['252858420309262338'] = 0.01, --Sage
+    ['277618305752367105'] = 0.06, --kaitlynn
+    ['254784266959847425'] = 0.01, --michael
+}
+
+--[[helper functions]]
+
+
+--[[actual commands]]
 local function roll(client, core, message)
 
 end
@@ -10,18 +23,30 @@ local function min(client, core, message)
 
 end
 
-local function acon(client, core, message)
-
+local function superon(client, core, message)
+    if whitelist[message.author.id] and message.channel.type==1 then
+        if tonumber(message._text) and tonumber(message._text)>=1 and tonumber(message._text)<=100 then
+            whitelist[message.author.id] = tonumber(message._text) / 100
+            message:reply("RNG cheat activated: You will now roll in the " .. message._text .. "th percentile.")
+        else
+            message:reply("I'm sorry. You must enter a number between 1 and 100 (inclusive) in order to activate this cheat.")
+        end
+    end
+    return nil
 end
 
-local function acoff(client, core, message)
-
+local function superoff(client, core, message)
+    if whitelist[message.author.id] and message.channel.type==1 then
+        whitelist[message.author.id] = whitelistDefaults[message.author.id]
+        message:reply("RNG cheat deactivated: You will now roll normally.")
+    end
+    return nil
 end
 
 return {
     roll = roll,
     max = max,
     min = min,
-    acon = acon,
-    acoff = acoff,
+    superon = superon,
+    superoff = superoff,
 }
